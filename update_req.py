93473dab2e198783f10bd0dd0517b9a54d52a440
TheTripleV <vasistavovveti@gmail.com>
2020-02-06 21:49:10 -0600

    prints and typo

--- update_req.py
+++ update_req.py
@@ -5,9 +5,15 @@ repo_name = sys.argv[1] # Ex. robotpy/robotpy-wpilib
 repo_path = "https://github.com/" + repo_name + ".git"
 package_name = sys.argv[2] # Ex. wpilib
 
+print('Repository Name:', repo_name)
+print('Repository Path:', repo_path)
+print('Package Name:', package_name)
+
 tag = git.Repo.clone_from(repo_path, "./temp", branch="master").git.describe('--tags', '--abbrev=0')
 
 lower_bound = tag
+
+print('Latest Version:', lower_bound)
 
 version = tag.split('.')
 
@@ -19,13 +25,18 @@ for i in range(len(version)):
 
 upper_bound = '.'.join(version)
 
+print('Next Major Version:', upper_bound)
+
 file_data = None
 with open("requirements.txt", "r") as file:
     file_data = file.readlines()
 
 for i in range(len(file_data)):
     if file_data[i].find(package_name + ">=") == 0:
+        print('Package found in requirements.txt')
         file_data[i] = "{}>={}, <{}\n".format(package_name, lower_bound, upper_bound)
 
+print('Rewriting Requirements')
 with open("requirements.txt", "w") as file:
     file.writelines(file_data)
+print('Done')
